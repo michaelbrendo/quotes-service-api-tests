749ae0f8e9d85c638a7343c8360affa49621de70
Michael Santos <23271123+michaelbrendo@users.noreply.github.com>
2026-09-24 21:13:22 -0300

    feat: create contract test and ajust JSON schema constraints

---
+++ src/test/kotlin/tests/CreateQuoteContractTest.kt
@@ -0,0 +1,39 @@
+package tests
+
+import io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath
+import mock.QuotesMockServer
+import model.CreateQuoteRequest
+import model.CreateQuoteRequestItem
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CreateQuoteContractTest : BaseTest() {
+
+    @Test
+    @DisplayName("Contract: Validate CreateQuoteResponse payload against JSON Schema")
+    fun shouldValidateCreateQuoteJsonSchema() {
+
+        QuotesMockServer.stubCreateQuoteAC1()
+
+        val requestPayload = CreateQuoteRequest(
+            customer = "Acme Corp",
+            items = listOf(
+                CreateQuoteRequestItem(
+                    item = "Product A",
+                    quantity = 2.0f,
+                    unitaryPrice = 50.0,
+                    discountPercentage = 0.0f
+                )
+            )
+        )
+
+
+        val response = quotesClient.postCreateQuote(requestPayload)
+
+        response.then().log().all()
+
+        response.then()
+            .assertThat()
+            .body(matchesJsonSchemaInClasspath("schemas/create-quote-response-schema.json"))
+    }
+}
